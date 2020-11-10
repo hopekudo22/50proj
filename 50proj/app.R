de1db2ec7fe586f58055ab3fa79f2fc1588acdc0
Hope Kudo <hopekudo@Hopes-MBP.hawaii.rr.com>
2020-11-10 13:46:34 -1000

    data wrangling part 1

--- 50proj/app.R
+++ 50proj/app.R
@@ -2,44 +2,53 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 library(atus)
+library(dplyr, warn.conflicts = FALSE)
+library(ggforce)
 
 #Load ATUS data
-atushours <- read.csv("atusact") 
+dtest <- read.csv("HI.csv")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- navbarPage(
+    "Final Project Title",
+    tabPanel("Home",
+             titlePanel("Home"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("Model",
+             fluidPage(
+                 selectInput("x", "X variable", choices = names(dtest)),
+                 selectInput("y", "Y variable", choices = names(dtest)),
+                 selectInput("geom", "geom", c("point", "column", "jitter")),
+                 plotOutput("plot")
+             )),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is where I talk about my project."),
+             h3("About Me"),
+             p("My name is Hope Kudo and I study Government. 
+             You can reach me at ______@college.harvard.edu.")))
 
-    # Application title
-    titlePanel("Analyzing Daily Routine"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+server <- function(input, output, session) {
+    plot_geom <- reactive({
+        switch(input$geom,
+               point = geom_point(),
+               #smooth = geom_smooth(se = TRUE, na.rm = TRUE),
+               jitter = geom_jitter()
+        )
     })
+    
+    output$plot <- renderPlot({
+        ggplot(dtest, aes(.data[[input$x]], .data[[input$y]])) +
+            plot_geom()
+    }, res = 96)
 }
 
 # Run the application 
