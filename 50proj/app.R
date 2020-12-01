df34ecdf128d15407ea54abc7703bb463d63148a
Hope Kudo <hopekudo@Hopes-MBP.hawaii.rr.com>
2020-12-01 10:44:43 -1000

    edits

--- 50proj/app.R
+++ 50proj/app.R
@@ -2,6 +2,8 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 library(atus)
+library(shinythemes)
+library(readr)
 library(dplyr, warn.conflicts = FALSE)
 library(ggforce)
 
@@ -11,11 +13,18 @@ model <- read.csv("2016.csv")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
+ 
+  theme = shinytheme("cyborg"),
     "How Americans Spend Time During the Day",
-    tabPanel("Home",
+    
+  tabPanel("Home",
              titlePanel("Home"),
-             p("I modelled the differences in time spent in different states.")),
-    tabPanel("Model",
+             p("I modelled the differences in time spent in different states. I chose to focus on how the average American spends their time. 
+             By looking at a wide range of activities such as how much someone sleeps in a day, the amount of housework they do, the time
+               they spendon sporting events, and so forth. The time data can be organized by state, family income,
+               education, age, and type of job.")),
+    
+  tabPanel("Model",
              h3("Working on data wrangling to fit the state level data to represent the averages, 
                 some states may appear higher right now since they are overrepresented in the sample."),
              fluidPage(
@@ -25,38 +34,45 @@ ui <- navbarPage(
                  #selectInput("geom", "geom", c("point", "column", "histogram", "jitter", "smooth")),
                  plotOutput("plot")
              )),
- #   tabPanel("State Comparison",
-  #           h3("Time Usage Based on State Level data"),
-   #          sidebarLayout(
-    #             sidebarPanel(
-     #                selectInput("select_state", "Select State",
-      #                           choices = state)
-       #          ),
-#    mainPanel(
- #       plotOutput("Plot1")
-  #      )
-   # )
-#    ),
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("I chose to focus on how the average American spends their time. By looking at a wide range of
-               activities such as how much someone sleeps in a day, the amount of housework they do, the time
-               they spendon sporting events, and so forth. The time data can be organized by state, family income,
-               education, age, and type of job.")),
-    tabPanel("About", 
-             titlePanel("About"),
-             h3("Project Background and Motivations"),
-             p("This project utilizes data from the American Time Use Survey (ATUS) that collects 
-               information on how respondents spend their time along with socioeconomic demographics.
-               Through data visualizations and analysis, we are able to take a deeper dive into how
-               Americans spend their time and how different variables such as income, location, or sex,
-               among others can influence time usage."),
-             h3("About Me"),
-             p("My name is Hope Kudo and I study Government. 
-             You can reach me at hopekudo@college.harvard.edu.")))
+ 
+   tabPanel("State Comparison",
+            fluidPage(
+              fluidRow(column(13,
+                       h3("Time Usage Distriubtions Based on State"),
+                       p("To further analyze the data, I filtered the information for specific states."))),
+              fluidRow(column(13,
+                       h4("[some type of] Regression by State"),
+                       selectizeInput(inputId = "stateInput",
+                                      label = "State",
+                                      choices = unique(data$state), #add data set
+                                      selected = "Hawaii"),
+                       plotOutput("Plot 2")
+                       )))),
+    
+    tabPanel("About",
+             fluidPage(
+               fluidRow(column(12,
+                               h3("Project Background"),
+                               p("This project utilizes data from the American Time Use Survey (ATUS) that collects 
+                                 information on how respondents spend their time along with socioeconomic demographics.
+                                 Through data visualizations and analysis, we are able to take a deeper dive into how
+                                 Americans spend their time and how different variables such as income, location, or sex,
+                                 among others can influence time usage."),
+                               h3("Source Information"),
+                               p("I used data from the American Time Usage Survey over the years of 2003 - 2017. I filtered
+                                 the data in order to make it easier to work with. [add more]"),
+                               h3("About Me"),
+                               p("My name is Hope Kudo and I'm a junior at Harvard studying Government and Psychology.
+                                 I can be reached at hopekudo@college.harvard.edu. The code for this project can be found at my",
+                                 a("GitHub page here.", href = "https://github.com/hopekud22/50proj"))))
+             )
+    )
+)
+        
 
 
-# Define server logic required to draw a histogram
+# Define server logic
+
 server <- function(input, output, session) {
     plot_geom <- reactive({
         switch(input$geom,
